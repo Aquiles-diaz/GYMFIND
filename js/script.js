4dc5e26f261ea06dd0cb33579b714581df6246d1
Aquiles-diaz <aquilesdiaz335@gmail.com>
2025-01-03 09:06:25 -0300

    3r commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const elements = document.querySelectorAll("#home, #tips, #services");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const elements = document.querySelectorAll("#home, #tips, #services");
 
-  // Función para detectar si el elemento está en la vista
-  function checkVisibility() {
-    elements.forEach((element) => {
-      const rect = element.getBoundingClientRect();
-      if (rect.top >= 0 && rect.top <= window.innerHeight * 0.75) {
-        element.classList.add("visible");
-      } else {
-        element.classList.remove("visible");
-      }
-    });
-  }
+//   // Función para detectar si el elemento está en la vista
+//   function checkVisibility() {
+//     elements.forEach((element) => {
+//       const rect = element.getBoundingClientRect();
+//       if (rect.top >= 0 && rect.top <= window.innerHeight * 0.75) {
+//         element.classList.add("visible");
+//       } else {
+//         element.classList.remove("visible");
+//       }
+//     });
+//   }
 
-  // Detectar el scroll
-  window.addEventListener("scroll", checkVisibility);
+//   // Detectar el scroll
+//   window.addEventListener("scroll", checkVisibility);
 
-  // Verificar la visibilidad al cargar la página
-  checkVisibility();
-});
+//   // Verificar la visibilidad al cargar la página
+//   checkVisibility();
+// });
